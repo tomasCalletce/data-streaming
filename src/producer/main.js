91b981a77b74237d9defa478e774a67946097c1a
StipGod <72163911+StipGod@users.noreply.github.com>
2023-11-21 13:31:42 -0500

    set up newsapi

--- src/producer/main.js
+++ src/producer/main.js
@@ -1,6 +1,5 @@
-const Kafka  = require('node-rdkafka');
-// import Kafka from 'node-rdkafka';
-// import eventType from '../eventType.js';
+const Kafka = require('node-rdkafka');
+const axios = require('axios');
 
 const stream = Kafka.Producer.createWriteStream({
   'metadata.broker.list': 'localhost:9092'
@@ -8,51 +7,37 @@ const stream = Kafka.Producer.createWriteStream({
   topic: 'test'
 });
 
-function queueMessage (){
-    const success = stream.write(Buffer.from('hi'));
-    if(success)
-        console.log('message succesfully written to stream');
-    else
-        console.log('something went wrong..');
+const NEWS_API_KEY = '10fb3fc6cd794d9085a56f42b506ab1b'; // Replace with your NewsAPI key
+const NEWS_API_URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=' + NEWS_API_KEY;
+
+async function fetchNewsHeadline() {
+  try {
+    const response = await axios.get(NEWS_API_URL);
+    const articles = response.data.articles;
+    if (articles.length > 0) {
+      return articles[0].title; 
+    }
+    return null;
+  } catch (error) {
+    console.error('Error fetching news:', error);
+    return null;
+  }
 }
-setInterval(()=>{
-    queueMessage();
-},3000)
 
-// stream.on('error', (err) => {
-//   console.error('Error in our kafka stream');
-//   console.error(err);
-// });
+async function queueMessage() {
+  const headline = await fetchNewsHeadline();
+  if (headline) {
+    const success = stream.write(Buffer.from(headline));
+    if (success) {
+      console.log('Message successfully written to stream:', headline);
+    } else {
+      console.log('Something went wrong..');
+    }
+  } else {
+    console.log('No headline to send');
+  }
+}
 
-// function queueRandomMessage() {
-//   const category = getRandomAnimal();
-//   const noise = getRandomNoise(category);
-//   const event = { category, noise };
-//   const success = stream.write(eventType.toBuffer(event));     
-//   if (success) {
-//     console.log(`message queued (${JSON.stringify(event)})`);
-//   } else {
-//     console.log('Too many messages in the queue already..');
-//   }
-// }
-
-// function getRandomAnimal() {
-//   const categories = ['CAT', 'DOG'];
-//   return categories[Math.floor(Math.random() * categories.length)];
-// }
-
-// function getRandomNoise(animal) {
-//   if (animal === 'CAT') {
-//     const noises = ['meow', 'purr'];
-//     return noises[Math.floor(Math.random() * noises.length)];
-//   } else if (animal === 'DOG') {
-//     const noises = ['bark', 'woof'];
-//     return noises[Math.floor(Math.random() * noises.length)];
-//   } else {
-//     return 'silence..';
-//   }
-// }
-
-// setInterval(() => {
-//   queueRandomMessage();
-// }, 3000);
+setInterval(() => {
+  queueMessage();
+}, 3000);
